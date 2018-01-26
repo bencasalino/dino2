64372caa1dc8942fb469b907ec0ecae1e6158da1
Ben Casalino <bencasalino@gmail.com>
2018-01-26 15:10:21 -0700

    dlkfjslkd

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+// this imports the header component
+import Header from "./components/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <Header />
+      
+    );
+  }
+}
+
+export default App;
